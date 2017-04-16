9c28b23a737d0ab1d606025d6155807baf900757
floDv1996 <floriandv1996@gmail.com>
2017-04-16 10:56:30 +0200

    Prepare for Javascript

---
+++ Javascript/animPhoto.js
@@ -0,0 +1,8 @@
+/*----Au survol de ma photo, l'utilisateur découvrira mes réseaux sociaux----*/
+
+$('#photo').mouseover(function(event) {
+	/* Act on the event */
+
+	$('#photo').opacity:0.5;
+
+});
